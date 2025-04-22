07dab1875e9e41f08169076295355023e6f32636
Kayla <90290561+Kk-byte319@users.noreply.github.com>
2025-04-21 20:05:53 -0400

    Updating login functions

--- employeeMS/src/App.jsx
+++ employeeMS/src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Login from './components/login'
 
 function App() {
-
   return (
-    <>THIS IS KAYLA</>
-
+    <><Login/></>
   )
 }
 
--- employeeMS/src/components/login.jsx
+++ employeeMS/src/components/login.jsx
@@ -1,28 +1,33 @@
-import React from "react"
-//import "bootstrap/dist/css/bootstrap.min.css"
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './style.css'
 
-const login = () => {
-
+const Login = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
-        <div className="p-3 rounded w-25 border loginForm">
+    <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
+        <div className='p-3 rounded w-25 border loginForm'>
             <h2>Employee Login Page</h2>
             <form>
-                <div>
+                <div className='mb-3'>
                     <label htmlFor="email"><strong>Email:</strong></label>
-                    <input type="email" name = "email" autoComplete="off" placeholder="Enter email here"
-                    className="form-control rounded-0" />
+                    <input type="email" name ='email' autoComplete='off' placeholder='Enter email here'
+                    className='form-control rounded-0' />
                 </div>
-                <div>
-                    <label htmlFor="password"><strong><Password></Password>:</strong></label>
-                    <input type="password" name = "password" placeholder="Enter Password"
-                    className="form-control rounded-0" />
+                <div className='mb-3'>
+                    <label htmlFor="password"><strong>Password:</strong></label>
+                    <input type="password" name = 'password' placeholder='Enter Password'
+                    className='form-control rounded-0' />
                 </div>
-                <button className = "btn btn-success w-100 rounded-0">Submit</button>
+                <button className = 'btn btn-success w-100 rounded-0 me-2'>Log In</button>
+                <div className='mb-1'>
+                    <input type="checkbox" name="tick" id = "tick" class/>
+                    <label htmlFor="password">Agree to the terms & conditions</label>
+                    
+                </div>
             </form>
         </div>
     </div>
   )
 }
 
-export default App
+export default Login
